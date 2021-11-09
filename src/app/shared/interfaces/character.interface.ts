fe55ad0f65f664734d0c1a421330319a2476ea0f
luisalfredosv <luissarabia80@gmail.com>
2021-11-09 16:08:23 -0400

    [create] Character interface

---
+++ src/app/shared/interfaces/character.interface.ts
@@ -0,0 +1,27 @@
+export interface ICharacter {
+  name: string;
+  alternate_names: string[];
+  species: string;
+  gender: string;
+  house: string;
+  dateOfBirth: string;
+  yearOfBirth: number;
+  wizard: boolean;
+  ancestry: string;
+  eyeColour: string;
+  hairColour: string;
+  wand: Wand;
+  patronus: string;
+  hogwartsStudent: boolean;
+  hogwartsStaff: boolean;
+  actor: string;
+  alternate_actors: string[];
+  alive: boolean;
+  image: string;
+}
+
+interface Wand {
+  wood: string;
+  core: string;
+  length: number;
+}
