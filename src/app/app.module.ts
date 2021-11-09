b859699c2c4cd37f09083d941723b4ee9f6be509
luisalfredosv <luissarabia80@gmail.com>
2021-11-09 14:37:59 -0400

    [update] Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [
