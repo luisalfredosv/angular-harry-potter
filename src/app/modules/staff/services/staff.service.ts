274d1251dbc66387f16e638b018b493319a1fc33
luisalfredosv <luissarabia80@gmail.com>
2021-11-09 15:30:55 -0400

    [update] new getStaff method

--- src/app/modules/staff/services/staff.service.ts
+++ src/app/modules/staff/services/staff.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { IStaff } from 'src/app/shared/interfaces/staff.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StaffService {
+  private readonly URL = environment.api;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getStaff$(): Observable<IStaff[]> {
+    return this.http.get(`${this.URL}/staff`).pipe(map((data: any) => data));
+  }
 }
