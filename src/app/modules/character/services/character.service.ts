7d02ca1f5347a43987bec84f2e41c730c26f104d
luisalfredosv <luissarabia80@gmail.com>
2021-11-10 10:05:17 -0400

    [refactoring] Implements interface ICharacter

--- src/app/modules/character/services/character.service.ts
+++ src/app/modules/character/services/character.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { EHouse } from 'src/app/shared/enum/house.enum';
+import { ICharacter } from 'src/app/shared/interfaces/character.interface';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -12,7 +13,7 @@ export class CharacterService {
 
   private URL = environment.api;
 
-  getCharacter$(house: EHouse): Observable<any> {
+  getCharacter$(house: EHouse): Observable<ICharacter[]> {
     return this.http
       .get(`${this.URL}/characters/house/${house}`)
       .pipe(map((data: any) => data));
