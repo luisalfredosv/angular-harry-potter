8aa5515a4177238dcc9826343f83654b5ef2ca41
luisalfredosv <luissarabia80@gmail.com>
2021-11-10 12:53:30 -0400

    [create] Component Shared table

---
+++ src/app/shared/components/table/table.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ICharacter } from '../../interfaces/character.interface';
+import { IStaff } from '../../interfaces/staff.interface';
+import { IStudent } from '../../interfaces/student.interface';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit {
+
+  @Input() listData : ICharacter[] | IStaff[] |  IStudent[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
