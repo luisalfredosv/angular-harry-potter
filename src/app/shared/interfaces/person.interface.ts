41607398c314ea5409b31841ae90d82d52837d6e
luisalfredosv <luissarabia80@gmail.com>
2021-11-10 12:54:29 -0400

    [create] Common interface Person

---
+++ src/app/shared/interfaces/person.interface.ts
@@ -0,0 +1,23 @@
+import { Wand } from "./wand.interface";
+
+export interface IPerson {
+    name: string;
+    alternate_names: string[];
+    species: string;
+    gender: string;
+    house: string;
+    dateOfBirth: string;
+    yearOfBirth: number;
+    wizard: boolean;
+    ancestry: string;
+    eyeColour: string;
+    hairColour: string;
+    wand: Wand;
+    patronus: string;
+    hogwartsStudent: boolean;
+    hogwartsStaff: boolean;
+    actor: string;
+    alternate_actors: string[];
+    alive: boolean;
+    image: string;
+}
