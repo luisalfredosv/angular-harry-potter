068daee816bb5cd760d78bb8eab2361f32d404d5
luisalfredosv <luissarabia80@gmail.com>
2021-11-10 12:55:06 -0400

    [update] extend common interface

--- src/app/shared/interfaces/staff.interface.ts
+++ src/app/shared/interfaces/staff.interface.ts
@@ -1,23 +1,3 @@
-import { Wand } from "./wand.interface";
+import { IPerson } from "./person.interface";
 
-export interface IStaff {
-  name: string;
-  alternate_names: string[];
-  species: string;
-  gender: string;
-  house: string;
-  dateOfBirth: string;
-  yearOfBirth: number;
-  wizard: boolean;
-  ancestry: string;
-  eyeColour: string;
-  hairColour: string;
-  wand: Wand;
-  patronus: string;
-  hogwartsStudent: boolean;
-  hogwartsStaff: boolean;
-  actor: string;
-  alternate_actors: string[];
-  alive: boolean;
-  image: string;
-}
+export interface IStaff extends IPerson {}
