2d512ce3b37e5dd162e2c5152fe1c2be60ce206e
luisalfredosv <luissarabia80@gmail.com>
2021-11-10 20:46:13 -0400

    [update] Importing StartModule

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () =>
+      import('../start/start.module').then((m) => m.StartModule),
+  },
+  {
     path: 'students',
     loadChildren: () =>
       import('../student/student.module').then((m) => m.StudentModule),
