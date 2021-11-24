8b0aca34e425dda7ff4480ada37c3eb036d685a9
luisalfredosv <luissarabia80@gmail.com>
2021-11-24 12:56:08 -0400

    [remove] Ngx pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SpinnerInterceptor } from './shared/interceptors/spinner.interceptor';
 import { HttpErrorInterceptor } from './shared/interceptors/httpError.interceptor';
-import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+// import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    MaterialModule
+    OverlayModule,
+    // FlexLayoutModule
+
   ],
   providers: [
     {
