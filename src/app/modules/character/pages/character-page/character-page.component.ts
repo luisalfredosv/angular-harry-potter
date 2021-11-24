38693d5dba7137f963d187e026341307ac462046
luisalfredosv <luissarabia80@gmail.com>
2021-11-24 12:58:57 -0400

    [update] CharaterPage - Implements Component Angular mat

--- src/app/modules/character/pages/character-page/character-page.component.ts
+++ src/app/modules/character/pages/character-page/character-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatOptionSelectionChange } from '@angular/material/core';
 import { Observable, of } from 'rxjs';
 import { EHouse } from 'src/app/shared/enum/house.enum';
 import { CharacterService } from '../../services/character.service';
@@ -37,8 +38,8 @@ export class CharacterPageComponent implements OnInit {
     this.listCharacters$ = this.characterService.getCharacter$(house);
   }
 
-  onChange(select: any) {
-    const house : EHouse = select.target.value;
+  onChange(select: MatOptionSelectionChange) {
+    const house : EHouse = select.source.value
     if(house){
       this.loading = true;
       this.getCharacters(house)
