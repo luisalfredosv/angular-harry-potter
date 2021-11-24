ba904c9cea87ef2a7d55e006e66156e734cf9ecb
luisalfredosv <luissarabia80@gmail.com>
2021-11-24 12:59:17 -0400

    [update] StudentPage - Implements Component Angular mat

--- src/app/modules/student/pages/student-page/student-page.component.ts
+++ src/app/modules/student/pages/student-page/student-page.component.ts
@@ -1,26 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { IPerson } from 'src/app/shared/interfaces/person.interface';
 import { StudentService } from '../../services/student.service';
 
 @Component({
   selector: 'app-student-page',
   templateUrl: './student-page.component.html',
-  styleUrls: ['./student-page.component.css']
+  styleUrls: ['./student-page.component.css'],
 })
 export class StudentPageComponent implements OnInit {
-
-  constructor(private studentService: StudentService) { }
-
+  constructor(private studentService: StudentService) {}
   listStudents$: Observable<any> = of([]);
 
   ngOnInit(): void {
-    this.getStudents()
+    this.getStudents();
   }
-
 
   getStudents(): void {
     this.listStudents$ = this.studentService.getStudents$();
   }
-
-
 }
