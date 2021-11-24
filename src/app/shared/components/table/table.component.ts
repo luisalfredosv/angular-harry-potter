e7479026476a2b846f4eb35fff5f03a0d06f5af8
luisalfredosv <luissarabia80@gmail.com>
2021-11-24 12:57:05 -0400

    [update] Table - Implements Component Angular mat

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,21 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ICharacter } from '../../interfaces/character.interface';
-import { IStaff } from '../../interfaces/staff.interface';
-import { IStudent } from '../../interfaces/student.interface';
+import { Component, Input, OnChanges, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnChanges {
+  dataSource = new MatTableDataSource();
 
-  @Input() listData : ICharacter[] | IStaff[] |  IStudent[] = [];
-  currentPage: number = 1;
+  @Input() listData = [];
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
-  constructor() { }
+  pageSize: number = 4;
+  pageNumber: number = 1;
+  pageSizeOptions: number[] = [5, 10, 20, 50, 100];
 
-  ngOnInit(): void {
+  displayedColumns: string[] = [
+    '#',
+    'name',
+    'species',
+    'gender',
+    'house',
+    'dateOfBirth',
+    'image',
+  ];
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
+  handlePage(event: PageEvent) {
+    this.pageSize = event.pageSize;
+    this.pageNumber = event.pageIndex + 1;
+  }
+
+  ngOnChanges() {
+    if (this.listData) {
+      this.dataSource.data = this.listData;
+    }
+  }
+
+  getIndex(index: any){
+   return index =
+      this.paginator.pageIndex > 0
+        ? index + this.paginator.pageIndex * this.paginator.pageSize
+        : index + 1;
+  }
 }
