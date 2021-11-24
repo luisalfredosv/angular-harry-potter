7d80810eb0d68bbe3919cc72a0dc441b9a3bd599
luisalfredosv <luissarabia80@gmail.com>
2021-11-24 12:57:45 -0400

    [update] MenuBar - Implements Component Angular mat

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface MenuItem {
+  label: string;
+  icon: string;
+  showOnMobile: boolean;
+  showOnTablet: boolean;
+  showOnDesktop: boolean;
+}
+
+
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
@@ -29,4 +38,49 @@ export class MenuBarComponent implements OnInit {
       },
     ];
   }
+
+  menuItems: MenuItem[] = [
+    {
+      label: 'Sign Up',
+      icon: 'login',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'About',
+      icon: 'help',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Pricing',
+      icon: 'attach_money',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Docs',
+      icon: 'notes',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: false
+    },
+  ];
 }
